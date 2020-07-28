6515f5a6cd4c6acee710bdb88663bb8b071bf8fb
LukeZvada <zvadamusic@gmail.com>
2020-07-28 13:32:17 -0500

    ride tickets and food tickets working

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,8 +1,8 @@
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener("click", (changeEvent) => { 
-    if (changeEvent.target.id === "rideTicket") {
+eventHub.addEventListener("click", (rideClick) => { 
+    if (rideClick.target.id === "rideTicket") {
     const rideEvent = new CustomEvent("rideTicketPurchased", {
         // detail: {
         //     event: changeEvent.target.value
@@ -12,10 +12,22 @@ eventHub.addEventListener("click", (changeEvent) => {
     }
 })
 
+eventHub.addEventListener("click", (foodClick) => { 
+    if (foodClick.target.id === "foodTicket") {
+    const foodEvent = new CustomEvent("foodTicketPurchased", {
+        // detail: {
+        //     event: changeEvent.target.value
+        // }
+        })
+        eventHub.dispatchEvent(foodEvent)
+    }
+})
+
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
             <button id="rideTicket">Ride Ticket</button>
+            <button id="foodTicket">Food Ticket</button> 
         </div>
     `
 }
